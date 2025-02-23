961551a686b9c2612747a56bae266ccc89d5498a
choward4 <chrishoward1337@gmail.com>
2025-02-22 23:27:36 -0600

    Added calculations, passing unit tests

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,15 @@ import numpy as np
 
 def calculate(list):
 
+    mean = []
+    variance = []
+    stdDev = []
+    max = []
+    min = []
+    sum = []
+    
     analysis = {
+        # forEach: [axis1, axis2, flattened]
         'mean': [],
         'variance': [],
         'standard deviation': [],
@@ -12,17 +20,39 @@ def calculate(list):
     }
 
     # Check if list meets standard, otherwise throw error
-    
-    try: 
-        if len(list) != 9:
-            raise ValueError
-    except ValueError as exp:
-        print("List must contain nine numbers.", exp)
-        exit()
-
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+        
     # convert list to 3x3 np array
-    # TODO
-    
+    flattenedArr = np.array(list)
+    Matrix = flattenedArr.reshape(3,3)
 
+
+    # add values to dict for axis1 and axis2 
+    for x in range(2):
+       mean.append(Matrix.mean(axis=x).tolist())
+       variance.append(Matrix.var(axis=x).tolist())
+       stdDev.append(Matrix.std(axis=x).tolist())
+       max.append(Matrix.max(axis=x).tolist())
+       min.append(Matrix.min(axis=x).tolist())
+       sum.append(Matrix.sum(axis=x).tolist())
+    
+    # add values to dict for flattened arr
+    mean.append(float(flattenedArr.mean()))
+    variance.append(float(flattenedArr.var()))
+    stdDev.append(float(flattenedArr.std()))
+    max.append(float(flattenedArr.max()))
+    min.append(float(flattenedArr.min()))
+    sum.append(float(flattenedArr.sum()))
+
+    # Update dictionary analysis
+    analysis['mean'] = mean
+    analysis['variance'] = variance
+    analysis['standard deviation'] = stdDev
+    analysis['max'] = max
+    analysis['min'] = min
+    analysis['sum'] = sum
+
+    #print(analysis)
 
     return analysis
